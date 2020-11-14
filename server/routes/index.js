bf61a27cb9119003047a11db76992b9a54b6486e
Nothing00Official <n.zuliani@nothmail.it>
2020-11-14 12:08:00 +0000

    pig

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Expresso', test: 'ciaoo', number: 23423, username: 'Nothing00', link: '/test', array: [
+      "elemento 1",
+      "elemento 2",
+      "elemento 3",
+      "elemento 4",
+      "elemento 5",
+      "elemento 6",
+  ] });
 });
+
+router.get('/test', function(req, res, next) {
+  res.render('test');
+});
+
 
 module.exports = router;
