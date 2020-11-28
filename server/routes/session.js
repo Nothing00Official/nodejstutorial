0b5b6cefb0eea698d4af9a73f9e2a6b710a0967f
nothing00official <nothing00@nothmail.it>
2020-11-28 13:24:01 +0100

    params

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const users = require('../users.json');
+
 router.post('/', function (req, res, next) {
-    res.render('account', { title: 'Prova', date: new Date() });
+    let username = req.body.username;
+    let email = req.body.email;
+    let psw = req.body.psw;
+    let rpsw = req.body.rpsw;
+
+    if (psw == rpsw) {
+        res.render('account', { title: 'Prova', date: new Date(), user: username, mail: email });
+    } else {
+        res.send("<p style=\"color:red\">Le password non coincidono!</p>")
+    }
+});
+
+router.get('/:email/:color', function (req, res, next) {
+    let mail = req.params.email;
+    let user = users.find(el => el.email == mail);
+    user["color"] = req.params.color;
+    res.render('user', user)
 });
 
 module.exports = router;
